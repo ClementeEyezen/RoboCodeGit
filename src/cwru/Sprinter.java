c1ff2df634bbb600465e7150df9afbe42c580249
William Baskin <mobile.wbaskin@gmail.com>
2013-10-18 12:37:34 -0400

    Sprinter Waves ready to test
    The code for choosing a place to move based on waves is ready to go, now
    it just needs to be tested

--- src/cwru/Sprinter.java
+++ src/cwru/Sprinter.java
@@ -11,6 +11,10 @@ public class Sprinter extends Legs implements Paintable
 	//Sprinter is a wave surfing movement extension of legs
 	ArrayList<WaveModel> surf_bum = new ArrayList<WaveModel>();
 	long oldTime;
+	double last_far_x = -1;
+	double last_far_y = -1;
+	double last_near_x = -1;
+	double last_near_y = -1;
 	public Sprinter(LifeBox source, cwruBase cwruBase) 
 	{
 		super(source, cwruBase);
@@ -28,6 +32,11 @@ public class Sprinter extends Legs implements Paintable
 		{
 			update(surf_bum.get(i),current_time);
 		}
+		double[] output = inner_points_test(long_points_test());
+		double desired_heading = Math.atan2(robot.getY()-output[1], robot.getX()-output[0]);
+		double current_heading = robot.getHeading();
+		moveEndTheta = desired_heading-current_heading;
+		moveEndDistance = Brain.distance(output[0], output[1], robot.getX(), robot.getY());
 		//TODO IDEAPAD
 		/*
 		 * The robot should test 32 points at radius of 50 around itself, and evaluate
@@ -45,12 +54,13 @@ public class Sprinter extends Legs implements Paintable
 		 * 	Use a move to point function to move to the interior point 
 		 * 	with the highest rating
 		 * 
-		 * 	TODO implement this first. It shouldn't be to hard, create a function that takes
-		 *  a point and tests it for wave overlap
-		 *  Then create a function that generates outer points, tests and returns the 
+		 * 	TODO implement this first. It shouldn't be to hard, 
+		 *  //DONE create a function that takes a point and counts wave overlap
+		 *  //DONE Then create a function that generates outer points, tests and returns the 
 		 *  	optimal outer point bearing
-		 *  From there, use a different function to generate random inner points (8<=d<50),
+		 *  //DONE From there, use a different function to generate random inner points (8<=d<50),
 		 *  	choose the best one, and then output the point to move to.
+		 *  TODO test
 		 */
 		//TODO Improvements
 		/*
@@ -117,6 +127,78 @@ public class Sprinter extends Legs implements Paintable
 				}
 			}
 		}
+	}
+	public int waves_over(double x, double y)
+	{
+		int current_waves = 0;
+		for (WaveModel wave : surf_bum)
+		{
+			double distance_early = Brain.distance(wave.early_origin_x, wave.early_origin_y,
+					x, y);
+			double distance_late = Brain.distance(wave.late_origin_x, wave.late_origin_y, 
+					x, y);
+			if (distance_early<=wave.early_radius && distance_late>=wave.late_radius)
+			{
+				current_waves++;
+			}
+		}
+		return current_waves;
+	}
+	public double long_points_test()
+	{
+		//returns the bearing to the optimal point 50 pixels away
+		double optimal_bearing = 0;
+		double optimal_function = -1;
+		double fraction = 32;
+		double distance = 50;
+		double increment = Math.PI/(fraction/2);
+		double this_x = robot.getX();
+		double this_y = robot.getY();
+		double this_heading = robot.getHeadingRadians();
+		for (int i = 0; i<fraction; i++)
+		{
+			double test_bearing = i*increment;
+			double test_x = distance*Math.cos(test_bearing)+this_x;
+			double test_y = distance*Math.sin(test_bearing)+this_y;
+			int wave_count = waves_over(test_x, test_y);
+			double delta_heading = test_bearing-this_heading;
+			double function = (1-Math.cos(delta_heading))/(2*(wave_count+1));
+			if (function > optimal_function)
+			{
+				optimal_bearing = test_bearing;
+				optimal_function = function;
+				last_far_x = test_x;
+				last_far_y = test_y;
+			}
+		}
+		double demathed_bearing = -optimal_bearing+Math.PI/2;
+		return optimal_bearing;
+	}
+	public double[] inner_points_test(double optimal_bearing)
+	{
+		double[] xyPoints = new double[2];
+		double this_x = robot.getX();
+		double this_y = robot.getY();
+		double optimal_function = -2;
+		for (int i = 0; i<8; i++)
+		{
+			double random_distance = Math.random()*(50-8)+8; //picks a random distance 8-50
+			double random_bearing = Math.random()*2*Math.PI-Math.PI; //rand bearing -pi to pi
+			double test_x = random_distance*Math.cos(random_bearing)+this_x;
+			double test_y = random_distance*Math.sin(random_bearing)+this_y;
+			int wave_count = waves_over(test_x, test_y);
+			double delta_heading = random_bearing-optimal_bearing;
+			double function = Math.cos(delta_heading)/(wave_count+1);
+			if (function > optimal_function)
+			{
+				optimal_function = function;
+				xyPoints[0] = test_x;
+				xyPoints[1] = test_y;
+				last_near_x = test_x;
+				last_near_y = test_y;
+			}
+		}
+		return xyPoints;
 	}
 	public void onPaint(Graphics2D g) 
 	{
