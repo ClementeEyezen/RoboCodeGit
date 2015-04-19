9aeaec2a4137dcb82981f504b4b4a272bcc736df
William Baskin <mobile.wbaskin@gmail.com>
2015-04-19 13:48:53 -0400

    MotionType now independent (so others can expand)

--- src/arc/model/MotionModel.java
+++ src/arc/model/MotionModel.java
@@ -12,6 +12,8 @@ public class MotionModel {
 	
 	private List<MotionType> models;
 	
+	private RobotModel parent;
+	
 	public MotionModel(RobotModel parent) {
 		// TODO constructor
 		// models/calculates the motions of the parent robot
@@ -22,28 +24,42 @@ public class MotionModel {
 		models = new ArrayList<MotionType>();
 		models.add(new StandStill());
 		most_likely = models.get(0);
+		this.parent = parent;
 	}
 	public void update() {
 		for(MotionType mt : models) {
-			
+			mt.update(parent.tc);
 		}
 	}
-	public void test(ScannedRobotEvent sre, TimeCapsule history) {
+	public void test(ScannedRobotEvent sre, TimeCapsule updated_history) {
 		
 		// Test models for fit
 		// TODO
 		/*
-		  sre.getBearingRadians() 
-          sre.getDistance() 
-          sre.getEnergy() 
-          sre.getHeadingRadians() 
-          sre.getName() 
-          sre.getVelocity() 
-          sre.getTime()
+			  sre.getBearingRadians() 
+	          sre.getDistance() 
+	          sre.getEnergy() 
+	          sre.getHeadingRadians() 
+	          sre.getName() 
+	          sre.getVelocity() 
+	          sre.getTime()
 		 */
+		if(!sre.getName().equals(parent.name)) return;
+		for(MotionType mt : models) {
+			mt.update_rating(updated_history);
+		}
 	}
-	public void test(AdvancedRobot ar, TimeCapsule history) {
+	public void test(TimeCapsule updated_history) {
 		// TODO test in the case of looking at self
+		/*
+		  	ar.getX() 
+        	ar.getY() 
+	        ar.getEnergy() 
+	        ar.getHeadingRadians() 
+	        ar.getName() 
+	        ar.getVelocity() 
+	        ar.getTime()
+		 */
 	}
 	
 	public double max_velocity(TimeCapsule.StateVector state) {
@@ -70,38 +86,21 @@ public class MotionModel {
 		}
 	}
 	
-	abstract class MotionType {
-		// project the motion of the robot forward using this motion type
-		// keeps track of projections and rating for the MotionType
-		ArrayList<Projection> past_projections;
-		double running_rating;
-		public void update(TimeCapsule tc) {
-			past_projections.add(project(tc,tc.current_time,20));
-		}
-		public abstract Projection project(TimeCapsule tc, long start_time, long time_forward);
-		public final double update_rating(TimeCapsule tc) {
-			double new_value = past_projections.remove(0).test(tc);
-			if(running_rating == 0) {
-				running_rating = new_value;
-			}
-			else {
-				running_rating = (running_rating*20+new_value)/(20+1);
-			}
-			return running_rating;
-		}
-	} 
 	class StandStill extends MotionType {
 		@Override
-		public Projection project(TimeCapsule tc, long start_time, long time_forward) {
+		public MotionProjection project(TimeCapsule tc, long start_time, long time_forward) {
 			double[] x = new double[(int)time_forward];
 			double[] y = new double[(int)time_forward];
 			long[] t = new long[(int)time_forward];
+			
+			TimeCapsule.StateVector start_data = tc.get_last(0);
+			
 			for(int i = 0; i < (int)time_forward; i++) {
-				x[i] = tc.data.get(new Integer((int)start_time)).x();
-				x[i] = tc.data.get(new Integer((int)start_time)).y();
+				x[i] = start_data.x();
+				x[i] = start_data.y();
 				t[i] = start_time + i;
 			}
-			return new Projection(x,y,t);
+			return new MotionProjection(x,y,t);
 		}
 	}
 }
---
+++ src/arc/model/MotionType.java
@@ -0,0 +1,24 @@
+package arc.model;
+
+import java.util.ArrayList;
+
+public abstract class MotionType {
+	// project the motion of the robot forward using this motion type
+	// keeps track of projections and rating for the MotionType
+	ArrayList<MotionProjection> past_projections;
+	double running_rating;
+	public void update(TimeCapsule tc) {
+		past_projections.add(project(tc,tc.current_time,20));
+	}
+	public abstract MotionProjection project(TimeCapsule tc, long start_time, long time_forward);
+	public final double update_rating(TimeCapsule tc) {
+		double new_value = past_projections.remove(0).test(tc);
+		if(running_rating == 0) {
+			running_rating = new_value;
+		}
+		else {
+			running_rating = (running_rating*20+new_value)/(20+1);
+		}
+		return running_rating;
+	}
+}
