fbb2ddcbd0a0903667cecec2e8e6a0a67b86627b
William Baskin <mobile.wbaskin@gmail.com>
2015-06-23 00:52:30 -0400

    OnePass projection added

--- src/arc/model/motions/OnePassCircular.java
+++ src/arc/model/motions/OnePassCircular.java
@@ -58,8 +58,29 @@ public class OnePassCircular extends MotionType {
 
 	@Override
 	public MotionProjection project(TimeCapsule tc, long time_forward) {
-		// TODO Auto-generated method stub
-		return null;
+		double[] x = new double[(int) time_forward];
+		double[] y = new double[(int) time_forward];
+		long[] t = new long[(int) time_forward];
+		
+		double h0 = tc.last().get(0).heading();
+		double dx = average_linear[(int) time_forward] * Math.cos(h0);
+		double dy = average_linear[(int) time_forward] * Math.sin(h0);
+		x[0] = tc.last().get(0).x();
+		y[0] = tc.last().get(0).y();
+		t[0] = tc.last_time();
+		
+		for(int i = 1; i < time_forward; i++) {
+			double new_vel = average_linear[(int) time_forward];
+			double new_hea = h0+i*average_angular[(int) time_forward];
+			System.out.println("new_vel: "+new_vel+" new_heading: "+new_hea);
+			dx = new_vel * Math.cos(new_hea);
+			dy = new_vel * Math.sin(new_hea);
+			x[i] = x[i-1]+dx;
+			y[i] = y[i-1]+dy;
+			t[i] = tc.last_time()+i;
+		}
+		
+		return new MotionProjection(x, y, t);
 	}
 
 }
