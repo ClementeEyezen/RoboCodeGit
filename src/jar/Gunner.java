90011cf022e12cd66321accc726c7e3787745b8f
Buck Baskin <mobile.wbaskin@gmail.com>
2016-04-23 17:39:16 -0400

    Reformat java code

--- src/jar/Gunner.java
+++ src/jar/Gunner.java
@@ -2,13 +2,13 @@ package jar;
 
 public class Gunner extends Actor {
 
-	public Gunner(Bot b) {}
-	
-	public double angular() {
-		return 0.0;
-	}
-	
-	@Override
-	public void update() {}
+    public Gunner(Bot b) {}
+
+    public double angular() {
+        return 0.0;
+    }
+
+    @Override
+    public void update() {}
 
 }
--- src/jar/Raddar.java
+++ src/jar/Raddar.java
@@ -2,18 +2,18 @@ package jar;
 
 public class Raddar extends Actor {
 
-	public Raddar(Bot b) {}
-	
-	public double angular() {
-		return 0.0;
-	}
-	
-	public boolean fire() {
-		return false;
-	}
-	
-	@Override
-	public void update() {}
+    public Raddar(Bot b) {}
+
+    public double angular() {
+        return 0.0;
+    }
+
+    public boolean fire() {
+        return false;
+    }
+
+    @Override
+    public void update() {}
 
 
 
